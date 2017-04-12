5b66da96fbe9b2e762a2ed7ef5889ac83dac8715
Kyle Feng <kylefeng28@gmail.com>
2017-04-12 01:57:05 -0400

    Add toposort

---
+++ graphs/toposort.cpp
@@ -0,0 +1,32 @@
+void topo_sort() {
+	queue<int> q; // candidates with indegree 0
+	int counter = 0;
+	int sorted[V];
+
+	// for each vertex v
+	for (int v = 0; v < V; v++) {
+		if (in_degs[v] == 0) { q.push(v); } // enqueue
+	}
+
+	while (!q.empty()) {
+		int v = q.front(); q.pop(); // dequeue
+		sorted[counter++] = v;
+
+		// for each vertex w adjacent to v
+		for (int w : edges[v])  {
+			if (--in_degs[w] == 0) { // remove edge (v, w) and check if w is a candidate
+				q.push(w); // enqueue
+			}
+		}
+	}
+
+	if (counter != V) {
+		throw; // Cycle found
+	}
+
+	// Print topo-sorted vertices
+	cout << "Topo sort:" << endl;
+	for (int i = 0; i < V; i++) {
+		cout << rev_map[sorted[i]] << " ";
+	}
+}
